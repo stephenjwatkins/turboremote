eb0b031bd421f8d94a4c1bf299b5eabce44a2640
Stephen Watkins <swatkins@easypost.com>
2022-10-17 22:46:58 -0400

    Add Sentry environment

--- apps/server/src/sentry.ts
+++ apps/server/src/sentry.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/node";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENV,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
